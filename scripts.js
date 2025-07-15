ac9ed93f1250674fc7d91dbd3798e414a8c6cffb
Azim's MacBook <buchkirex@gmail.com>
2025-07-15 14:45:24 -0500

    trying to make scroll-snap

--- scripts.js
+++ scripts.js
@@ -1,47 +1,14 @@
 const body = document.getElementsByTagName("body")[0];
 const hyperlinks = document.getElementsByTagName("a");
+window.addEventListener('scroll', function() {
+  const frontpage = document.getElementById('frontpage');
+  const video = document.getElementById('bg-video');
+  const rect = frontpage.getBoundingClientRect();
 
-function setAnchorColor(color) {
-    Array.from(hyperlinks).forEach(link => {
-        link.style.color = color;
-    })
-}
-
-
-function setColorSolid(name) {
-    body.style.background = null;
-    body.style.backgroundColor = name;
-    if (name === "blue"){
-        setAnchorColor("white");
-    }
-    else {
-        setAnchorColor("blue");
-    }
-}
-
-function setColorGradient(color1, color2){
-    body.style.backgroundColor = null;
-    body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
-    setAnchorColor("blue");
-}
-
-function randomColor(){
-    const red = Math.round(Math.random() * 255);
-    const green = Math.round(Math.random() * 255);
-    const blue = Math.round(Math.random() * 255);
-    const color = `rgb(${red}, ${green}, ${blue})`;
-    setColorSolid(color);
-}
-
-function randomGradient(){
-    const red1 = Math.round(Math.random() * 255);
-    const green1 = Math.round(Math.random() * 255);
-    const blue1 = Math.round(Math.random() * 255);
-    const color1 = `rgb(${red1}, ${green1}, ${blue1})`;
-    const red2 = Math.round(Math.random() * 255);
-    const green2 = Math.round(Math.random() * 255);
-    const blue2 = Math.round(Math.random() * 255);
-    const color2 = `rgb(${red2}, ${green2}, ${blue2})`;
-    setColorGradient(color1, color2);
-}
-
+  // If the top section is out of view, hide the video
+  if (rect.bottom <= 0) {
+    video.style.display = 'none';
+  } else {
+    video.style.display = 'block';
+  }
+});
